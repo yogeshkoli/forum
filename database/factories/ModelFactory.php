30a8a9f38585cc9fcebde7200ac83a9bacc09a5b
Yogesh Koli <info@yogeshkoli.com>
2017-06-09 18:43:43 +0530

    Database Migration for Threads and Reply along with Model Factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,32 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+
+$factory->define(App\Thread::class, function ($faker) {
+    return [
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'title'   => $faker->sentence,
+        'body'    => $faker->paragraph
+    ];
+});
+
+$factory->define(App\Reply::class, function ($faker) {
+    return [
+        'thread_id' => function () {
+            return factory('App\Thread')->create()->id;
+        },
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'body'      => $faker->paragraph
     ];
 });
